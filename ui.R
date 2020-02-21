c2d88085160e2212a912c060ba347d4740a1a184
Mihai Berbec <berbec.mihaita@gmail.com>
2020-02-22 01:29:37 +0200

    update shiny app

--- ui.R
+++ ui.R
@@ -1,128 +1,150 @@
 
-# Use ShinyDashboard
+
 library(shinydashboard)
 library(dplyr)
 library(readr)
 
-data <- read_csv("data/raw_water_data.csv")
-brands <- data$Brand
+brands <- read_csv("data/raw_water_data.csv") %>% 
+    pull(Brand)
+
 
 ui <- dashboardPage(
     
-    # Define Header and Sidebar
-    dashboardHeader(title = "Water for Coffee",
-                    titleWidth = 250),
+    dashboardHeader(title = "Water for Coffee", titleWidth = 230),
     
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Manual recipe calculator", tabName = "details", icon = icon("calculator")),
-            menuItem("Find best recipes", tabName = "leads", icon = icon("bar-chart"))
+            menuItem("Manual recipe calculator", tabName = "manual_calc", icon = icon("calculator")),
+            menuItem("Find target recipes", tabName = "target_calc", icon = icon("bar-chart"))
         )
     ),
     
     
     dashboardBody(
         
-        # Define content for each page
         tabItems(
             
-            # Find best recipes
-            tabItem(tabName = "leads",
-                    h2("Leads"),
-                    dataTableOutput("leads_table")
-            ),
-            
-            
-            # Manual recipe calculator
-            tabItem(tabName = "details",
+            tabItem(tabName = "manual_calc",
+                    h2("Manual recipe calculator"),
+                    br(),
+                    h4("Select bottled waters from the dropdown menu and input their proportions."),
                     
-                    # first row: inputs
                     fluidRow(
-                        # Define CSS style for the "Calculate" button
+                        
                         tags$head(tags$style(HTML('
                               .estimation_button {background-color: #33CE67; width: 100%}
                               .estimation_button:hover {background-color: #1DAA4C}
                               '))),
+                        
                         box(
-                            width = 2,
+                            width = 3,
                             title = "First water",
                             status = "primary",
                             solidHeader = TRUE,
-                            
-                            # input the party id as text
-                            selectInput("lead_party", label = NULL, choices = brands, selected = "Distilled water")
+                            selectInput("first_water", label = NULL, choices = brands, selected = "Distilled water"),
+                            numericInput("first_coef", label = "Proportion %", 40, min = 0, max = 100, step = 1)
                         ),
+                        
                         box(
-                            width = 2,
+                            width = 3,
                             title = "Second water",
                             status = "primary",
                             solidHeader = TRUE,
-                            
-                            # input the party id as text
-                            selectInput("lead_party", label = NULL, choices = brands, selected = "Distilled water")
+                            selectInput("second_water", label = NULL, choices = brands, selected = "Distilled water"),
+                            numericInput("second_coef", label = "Proportion %", 40, min = 0, max = 100, step = 1)
                         ),
+                        
                         box(
-                            width = 2,
+                            width = 3,
                             title = "Third water",
                             status = "primary",
                             solidHeader = TRUE,
-                            
-                            # input the party id as text
-                            selectInput("lead_party", label = NULL, choices = brands, selected = "Distilled water")
-                        ),
-                        box(
-                            width = 2,
-                            title = "Fourth water",
-                            status = "primary",
-                            solidHeader = TRUE,
-                            
-                            # input the party id as text
-                            selectInput("lead_party", label = NULL, choices = brands, selected = "Distilled water")
+                            selectInput("third_water", label = NULL, choices = brands, selected = "Distilled water"),
+                            numericInput("third_coef", label = "Proportion %", 20, min = 0, max = 100, step = 1)
                         ),
                         
                         box(
-                            width = 2,
-                            title = "Calculate recipe",
+                            width = 3,
                             status = "success",
-                            solidHeader = FALSE,
-
-                            actionButton("calculate_recipe", icon("play"), class = "estimation_button")
+                            solidHeader = TRUE,
+                            actionButton("calc_recipe", label =  "Calculate recipe",  class = "estimation_button"),
+                            actionButton("save_recipe", label =  "Save to CSV", class = "estimation_button")
+                            
                         )
                     ),
                     
-                    
-                    # second row: recipe key info
+
                     fluidRow(
                         
-                        # Key summary info
-                        valueBoxOutput("source_info", width = 2),
-                        valueBoxOutput("account_info", width = 2),
-                        valueBoxOutput("credit_info", width = 2),
-                        valueBoxOutput("hist_info", width = 2)
+                        valueBoxOutput("alkalinity", width = 2),
+                        valueBoxOutput("hardness", width = 2),
+                        valueBoxOutput("magnesium", width = 2),
+                        valueBoxOutput("calcium", width = 2),
+                        valueBoxOutput("tds", width = 2),
+                        valueBoxOutput("ph", width = 2)
                     ),
                     
-                    # third row: charts
+                    
                     fluidRow(
                         
-                        # Add box for graph 
                         box(
-                            title = "Recipe details",
+                            title = "Compare to ingredients",
                             solidHeader = TRUE,
                             status = "primary",
                             width = 6,
-                            plotOutput("hist_purch_plot")
+                            plotOutput("plot_ingredients")
                         ),
                         
-                        # Add box for graph 
                         box(
                             title = "Compare to SCA standards",
                             solidHeader = TRUE,
                             status = "primary",
-                            width = 4,
-                            plotOutput("time_purch_plot")
+                            width = 6,
+                            plotOutput("plot_sca_range")
                         )
                     )
+            ),
+            
+
+            tabItem(tabName = "target_calc",
+                    h2("Recipes with specific alkalinity and hardness"),
+                    br(),
                     
+                    fluidRow(
+                        
+                        box(
+                            width = 3,
+                            title = "Alkalinity",
+                            status = "primary",
+                            solidHeader = TRUE,
+                            numericInput("target_alk", label = NULL, 40, min = 0, max = 120, step = 10)
+                        ),
+                        
+                        box(
+                            width = 3,
+                            title = "Hardness",
+                            status = "primary",
+                            solidHeader = TRUE,
+                            numericInput("target_hard", label = NULL, 68, min = 0, max = 220, step = 10)
+                        ),
+                        
+                        box(
+                            width = 3,
+                            status = "success",
+                            solidHeader = TRUE,
+                            actionButton("calc_recipe", label =  "Find recipes",  class = "estimation_button")
+                            
+                        )
+                    ),
+                    
+                    
+                    fluidRow(
+                        box(
+                            width = 9,
+                            DT::DTOutput("leads_table")
+                        )
+                    )
+        
             )
         )
     )
